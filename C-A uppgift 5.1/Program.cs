43fefa068f29f3cfe2321fffd80382d9be9ceb66
Pingvu <nhlpool27@gmail.com>
2023-02-02 13:42:13 +0100

    Add project files.

---
+++ C-A uppgift 5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] städer = new string[3];
+
+            städer[0] = "Stockhol";
+
+            städer[1] = "Göteborg";
+
+            städer[2] = "Malmö";
+
+
+            for (int i = 0; i < städer.Length; i++)
+            {
+                Console.WriteLine(städer[i] + "\n");
+            }
+
+
+        }
+    }
+}
+
